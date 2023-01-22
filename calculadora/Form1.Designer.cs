8ad95531f5110315ee42e0bf565a04c4e30d444f
HaroldMateo22 <20221719@gmail.com>
2023-01-22 11:53:33 -0400

    prueba 3

--- calculadora/Form1.Designer.cs
+++ calculadora/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(162, 393);
+            this.button2.Location = new System.Drawing.Point(149, 393);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 79);
             this.button2.TabIndex = 1;
