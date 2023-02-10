1aef270d2e7d6b5c61f982f1d9ac3f3ebf5a4269
HaroldMateo22 <113707043+HaroldMateo22@users.noreply.github.com>
2023-02-10 16:54:09 -0400

    Lotto 2

---
+++ Loteria intento 2/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Loteria_intento_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btngenerar = new System.Windows.Forms.Button();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtnum3 = new System.Windows.Forms.TextBox();
+            this.btncomparar = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtmanual1 = new System.Windows.Forms.TextBox();
+            this.txtmanual2 = new System.Windows.Forms.TextBox();
+            this.txtmanual3 = new System.Windows.Forms.TextBox();
+            this.lblpremio = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btngenerar
+            // 
+            this.btngenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngenerar.Location = new System.Drawing.Point(305, 245);
+            this.btngenerar.Name = "btngenerar";
+            this.btngenerar.Size = new System.Drawing.Size(143, 67);
+            this.btngenerar.TabIndex = 0;
+            this.btngenerar.Text = "Generar";
+            this.btngenerar.UseVisualStyleBackColor = true;
+            this.btngenerar.Click += new System.EventHandler(this.btngenerar_Click);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum1.Location = new System.Drawing.Point(68, 141);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.ReadOnly = true;
+            this.txtnum1.Size = new System.Drawing.Size(146, 22);
+            this.txtnum1.TabIndex = 1;
+            this.txtnum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum2.Location = new System.Drawing.Point(305, 141);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.ReadOnly = true;
+            this.txtnum2.Size = new System.Drawing.Size(143, 22);
+            this.txtnum2.TabIndex = 2;
+            this.txtnum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtnum3
+            // 
+            this.txtnum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum3.Location = new System.Drawing.Point(527, 141);
+            this.txtnum3.Name = "txtnum3";
+            this.txtnum3.ReadOnly = true;
+            this.txtnum3.Size = new System.Drawing.Size(149, 22);
+            this.txtnum3.TabIndex = 3;
+            this.txtnum3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btncomparar
+            // 
+            this.btncomparar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncomparar.Location = new System.Drawing.Point(305, 343);
+            this.btncomparar.Name = "btncomparar";
+            this.btncomparar.Size = new System.Drawing.Size(143, 81);
+            this.btncomparar.TabIndex = 4;
+            this.btncomparar.Text = "Comparar";
+            this.btncomparar.UseVisualStyleBackColor = true;
+            this.btncomparar.Click += new System.EventHandler(this.btncomparar_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(191, 35);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(149, 59);
+            this.lblresultado.TabIndex = 5;
+            this.lblresultado.Text = "Resultado";
+            this.lblresultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtmanual1
+            // 
+            this.txtmanual1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmanual1.Location = new System.Drawing.Point(68, 194);
+            this.txtmanual1.Name = "txtmanual1";
+            this.txtmanual1.Size = new System.Drawing.Size(146, 22);
+            this.txtmanual1.TabIndex = 6;
+            this.txtmanual1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtmanual2
+            // 
+            this.txtmanual2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmanual2.Location = new System.Drawing.Point(305, 194);
+            this.txtmanual2.Name = "txtmanual2";
+            this.txtmanual2.Size = new System.Drawing.Size(146, 22);
+            this.txtmanual2.TabIndex = 7;
+            this.txtmanual2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtmanual3
+            // 
+            this.txtmanual3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmanual3.Location = new System.Drawing.Point(527, 194);
+            this.txtmanual3.Name = "txtmanual3";
+            this.txtmanual3.Size = new System.Drawing.Size(146, 22);
+            this.txtmanual3.TabIndex = 8;
+            this.txtmanual3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblpremio
+            // 
+            this.lblpremio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpremio.Location = new System.Drawing.Point(425, 35);
+            this.lblpremio.Name = "lblpremio";
+            this.lblpremio.Size = new System.Drawing.Size(146, 59);
+            this.lblpremio.TabIndex = 9;
+            this.lblpremio.Text = "Premio";
+            this.lblpremio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ingrese el numero 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(305, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese el numero 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(524, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ingrese el numero 3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(68, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ganador 1";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(305, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ganador 2";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(524, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Ganador 3";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblpremio);
+            this.Controls.Add(this.txtmanual3);
+            this.Controls.Add(this.txtmanual2);
+            this.Controls.Add(this.txtmanual1);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btncomparar);
+            this.Controls.Add(this.txtnum3);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.btngenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btngenerar;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtnum3;
+        private System.Windows.Forms.Button btncomparar;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtmanual1;
+        private System.Windows.Forms.TextBox txtmanual2;
+        private System.Windows.Forms.TextBox txtmanual3;
+        private System.Windows.Forms.Label lblpremio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Loteria intento 2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loteria_intento_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngenerar_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num1 = rand.Next(1, 10);
+            int num2 = rand.Next(1, 10);
+            int num3 = rand.Next(1, 10);
+            txtnum1.Text = num1.ToString();
+            txtnum2.Text = num2.ToString();
+            txtnum3.Text = num3.ToString();
+        }
+
+        private void btncomparar_Click(object sender, EventArgs e)
+        {
+            int numacertados = 0;
+            int premio = 0;
+            int num1 = int.Parse(txtnum1.Text);
+            int num2 = int.Parse(txtnum2.Text);
+            int num3 = int.Parse(txtnum3.Text);
+            int nummanual = int.Parse(txtmanual1.Text);
+            int nummanual2 = int.Parse(txtmanual2.Text);
+            int nummanual3 = int.Parse(txtmanual3.Text);
+
+
+            if(txtmanual1.Text == txtnum1.Text || txtmanual1.Text == txtnum2.Text || txtmanual1.Text == txtnum3.Text)
+            {
+                numacertados++;
+            }
+            if (txtmanual2.Text == txtnum1.Text || txtmanual2.Text == txtnum2.Text || txtmanual2.Text == txtnum3.Text)
+            {
+                numacertados++;
+            }
+            if (txtmanual3.Text == txtnum1.Text || txtmanual3.Text == txtnum2.Text || txtmanual3.Text == txtnum3.Text)
+            {
+                numacertados++;
+            }
+
+
+            if (numacertados == 1)
+            {
+                lblresultado.Text = "acertaste " + numacertados.ToString() + " numero";
+            }
+            else
+            {
+                lblresultado.Text = "acertaste " + numacertados.ToString() + " numeros";
+            }
+
+
+            if (numacertados == 0)
+            {
+                lblpremio.Text = "Te Pelaste";
+            }
+            if (numacertados == 1)
+            {
+                lblpremio.Text = "Ganaste 2,000";
+            }
+            if (numacertados == 2)
+            {
+                lblpremio.Text = "Ganaste 5,000";
+            }
+            if (numacertados == 3)
+            {
+                lblpremio.Text = "Ganaste 10,000";
+            }
+            numacertados = 0;
+            premio = 0;
+        }
+    }
+}
