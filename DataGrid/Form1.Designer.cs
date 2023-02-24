a2911a62b3fe061f8084591184b827cd40716a6d
HaroldMateo22 <20221719@gmail.com>
2023-02-23 21:49:54 -0400

    DataGridView

---
+++ DataGrid/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvproductos = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.lblprecio = new System.Windows.Forms.Label();
+            this.lblinformacion = new System.Windows.Forms.Label();
+            this.btnadicionar = new System.Windows.Forms.Button();
+            this.btnborrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvproductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvproductos
+            // 
+            this.dtgvproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvproductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre,
+            this.Precio});
+            this.dtgvproductos.Location = new System.Drawing.Point(12, 256);
+            this.dtgvproductos.Name = "dtgvproductos";
+            this.dtgvproductos.Size = new System.Drawing.Size(388, 182);
+            this.dtgvproductos.TabIndex = 0;
+            this.dtgvproductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvproductos_CellClick);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo Producto";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre Producto";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio Producto";
+            this.Precio.Name = "Precio";
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(12, 25);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblcodigo.TabIndex = 1;
+            this.lblcodigo.Text = "Codigo";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(79, 25);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 2;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(79, 51);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 4;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(12, 51);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(44, 13);
+            this.lblnombre.TabIndex = 3;
+            this.lblnombre.Text = "Nombre";
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(79, 77);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 6;
+            // 
+            // lblprecio
+            // 
+            this.lblprecio.AutoSize = true;
+            this.lblprecio.Location = new System.Drawing.Point(12, 77);
+            this.lblprecio.Name = "lblprecio";
+            this.lblprecio.Size = new System.Drawing.Size(37, 13);
+            this.lblprecio.TabIndex = 5;
+            this.lblprecio.Text = "Precio";
+            // 
+            // lblinformacion
+            // 
+            this.lblinformacion.AutoSize = true;
+            this.lblinformacion.Location = new System.Drawing.Point(108, 131);
+            this.lblinformacion.Name = "lblinformacion";
+            this.lblinformacion.Size = new System.Drawing.Size(35, 13);
+            this.lblinformacion.TabIndex = 7;
+            this.lblinformacion.Text = "label1";
+            // 
+            // btnadicionar
+            // 
+            this.btnadicionar.Location = new System.Drawing.Point(262, 30);
+            this.btnadicionar.Name = "btnadicionar";
+            this.btnadicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnadicionar.TabIndex = 8;
+            this.btnadicionar.Text = "Adicionar";
+            this.btnadicionar.UseVisualStyleBackColor = true;
+            this.btnadicionar.Click += new System.EventHandler(this.btnadicionar_Click);
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Location = new System.Drawing.Point(262, 60);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(75, 20);
+            this.btnborrar.TabIndex = 9;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 457);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.btnadicionar);
+            this.Controls.Add(this.lblinformacion);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.lblprecio);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.lblcodigo);
+            this.Controls.Add(this.dtgvproductos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvproductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvproductos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.Label lblcodigo;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Label lblprecio;
+        private System.Windows.Forms.Label lblinformacion;
+        private System.Windows.Forms.Button btnadicionar;
+        private System.Windows.Forms.Button btnborrar;
+    }
+}
+
---
+++ DataGrid/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGrid
+{
+    public partial class Form1 : Form
+    {
+        private int n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadicionar_Click(object sender, EventArgs e)
+        {
+            int n = dtgvproductos.Rows.Add();
+
+            dtgvproductos.Rows[n].Cells[0].Value = txtcodigo.Text;
+            dtgvproductos.Rows[n].Cells[1].Value = txtnombre.Text;
+            dtgvproductos.Rows[n].Cells[2].Value = txtprecio.Text;
+
+            txtcodigo.Text = "";
+            txtnombre.Text = "";
+            txtprecio.Text = "";
+        }
+
+        private void dtgvproductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            n = e.RowIndex;
+            if (n != -1)
+            {
+                lblinformacion.Text = (string)dtgvproductos.Rows[n].Cells[1].Value;
+            }
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1)
+            {
+                dtgvproductos.Rows.RemoveAt(n);
+            }
+        }
+    }
+}
